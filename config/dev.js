343aeb0ee4f2a8445b90af1bfab0adf31fe2644b
Guilherme <willsaibott@gmail.com>
2018-07-28 17:13:37 -0300

    Removing dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://gbordignon:password1@ds129821.mlab.com:29821/advanced_node',
-  cookieKey: '123123123',
-  redisUrl: "redis://127.0.0.1:6379"
-};
